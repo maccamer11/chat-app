cc0ec84dc19007a0eb450930bdd87787c880319b
Das <maccameron@live.ca>
2020-01-03 22:05:18 -0700

    message function added basic

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,57 @@
 const socket = io()
 
+//elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $navigatorButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    $messageFormButton.setAttribute('disabled', 'disabled')
+    //disable
     const message = e.target.elements.message.value
-    socket.emit('sendMessage', message)
+
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        //enable
+        if (error) {
+            return console.log(error)
+        }
+        console.log('The message was delivered')
+    })
+})
+
+
+$navigatorButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+    //disable
+    $navigatorButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            $navigatorButton.removeAttribute('disabled')
+            console.log('Location shared!')
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -17,11 +18,27 @@ io.on('connection', (socket) => {
     console.log('New socket connection')
 
     socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined the chat')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, cb) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return cb('Profanity is not allowed!')
+        }
         io.emit('message', message)
+        cb('Message Delivered!')
     })
 
+    socket.on('sendLocation', (coords, cb) => {
+        io.emit('message', `https://www.google.com/maps/?q=${coords.latitude},${coords.longitude}`)
+
+        cb()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat')
+    })
 })
 
 server.listen(port, () => {
